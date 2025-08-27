9d510f64ace995ae4dcdf12e471c1282247c5f6e
best2025j <audugodstime95@gmail.com>
2025-08-28 00:46:15 +0100

    feat: modal added

--- apps/web/app/(dashboard)/guest/pass/page.tsx
+++ apps/web/app/(dashboard)/guest/pass/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@sambha/ui/button";
 import { Download } from "lucide-react";
 import { useState } from "react";
 import GlobalModal from "@sambha/ui/modal/globalModal";
-import avatar from "assets/images/avatar2.png";
+import avatar from "../../../../public/Images/oliver-emily.png";
 
 import Image from "next/image";
 import NotificationSwitch from "components/event-pass/NotificationSwitch";
@@ -22,6 +22,15 @@ const passes = [
     time: "8:30 PM",
     name: "Jane Smith",
   },
+];
+
+const users = [
+  { id: 1, name: "Alice book", img: avatar, host: true },
+  { id: 2, name: "Bob mark", img: avatar, host: true },
+  { id: 3, name: "Charlie", img: avatar },
+  { id: 4, name: "Cody Fisher", img: avatar },
+  { id: 5, name: "Ralph Edwards", img: avatar },
+  { id: 6, name: "you", img: avatar },
 ];
 
 export default function Passpage() {
@@ -40,14 +49,20 @@ export default function Passpage() {
         {passes.map((pass, index) => (
           <div key={index} className="flex flex-col gap-4">
             {/* Event pass card */}
-            <button onClick={toggleModal}>
+            <div
+              onClick={toggleModal}
+              role="button"
+              tabIndex={0}
+              onKeyDown={(e) => e.key === "Enter" && toggleModal()}
+              className="clickable-div"
+            >
               <EventPass
                 date={pass.date}
                 image="assets/images/event-pass-bg.png"
                 time={pass.time}
                 name={pass.name}
               />
-            </button>
+            </div>
 
             {/* Download button for each card */}
             <div className="flex justify-end">
@@ -64,49 +79,92 @@ export default function Passpage() {
       </div>
 
       {/*  */}
-      <div>
-        <h1>test modal</h1>
-        {/* Modal for Event Creation */}
-        <GlobalModal
-          isOpen={isModalOpen}
-          showCloseButton
-          onOpenChange={toggleModal}
-          preventOutsideClick={true}
-        >
-          <div className="w-full -2 h-full">
-            <div className="flex flex-col w-full space-y-4 md:w-[511px]">
-              <div className="flex pt-8 flex-col items-center space-y-2 w-full">
-                <Image
-                  src={avatar}
-                  alt="Avatar"
-                  width={500}
-                  height={500}
-                  className="rounded-full h-24 w-24"
-                />
-                <div className="py-4 flex flex-col items-center ">
-                  <h2 className="text-lg">Racoon Musicals</h2>
-                  <div className="text-base flex gap-2 ">
-                    <span> Wed, May 31</span> <span>12:00 PM</span>
-                  </div>
-                </div>
+      {/* Modal for Event Creation */}
+      <GlobalModal
+        isOpen={isModalOpen}
+        showCloseButton
+        onOpenChange={toggleModal}
+        preventOutsideClick={true}
+      >
+        <div className="w-full -2 h-full">
+          <div className="flex flex-col w-full space-y-4 md:w-[511px]">
+            <div className="flex pt-8 flex-col items-center space-y-2 w-full">
+              <Image
+                src={avatar}
+                alt="Avatar"
+                quality={100}
+                width={500}
+                height={500}
+                className="
+            rounded-full h-24 w-24 
+            object-cover
+            border-4 border-white shadow-md 
+            hover:scale-105 transition-transform duration-300
+            ring-2 ring-offset-2 ring-gray-500
+          "
+              />
 
-                <Button variant="secondary" className="mt-4 w-full font-medium">
-                  View event
-                </Button>
+              <div className="py-4 flex flex-col items-center ">
+                <h2 className="text-lg font-[Fractul] font-medium">
+                  Racoon Musicals
+                </h2>
+                <div className=" flex items-center  gap-2 ">
+                  <h1 className="text-sm md:text-lg font-bold bg-gradientText bg-clip-text text-transparent">
+                    Wed, May 31
+                  </h1>
+                  <span className="">*</span>
+                  <span className="text-gray-base">12:00 PM</span>
+                </div>
               </div>
 
-              <div className="flex space-y-3 flex-col">
-                <div className="flex justify-between items-center">
-                  <h6>Mute notifications</h6>
-                  <NotificationSwitch />
-                </div>
-                <h6 className="text-error-base">Report listing</h6>
+              <Button variant="secondary" className="mt-4 w-full font-medium">
+                View event
+              </Button>
+            </div>
+
+            <div className="flex space-y-3 flex-col">
+              <div className="flex justify-between items-center">
+                <h6>Mute notifications</h6>
+                <NotificationSwitch />
+              </div>
+              <h6 className="text-error-base">Report listing</h6>
+              <div className="flex justify-between items-center border-b pb-4">
                 <h6>Members</h6>
+                <span className="text-gray-500">23</span>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex flex-col space-y-3">
+                {users.map((user) => (
+                  <div
+                    key={user.id}
+                    className="flex items-center justify-between space-x-4"
+                  >
+                    <div className="flex items-center gap-3">
+                      <Image
+                        src={user.img}
+                        alt={user.name}
+                        width={28}
+                        height={28}
+                        className="rounded-full h-7 w-7 object-cover border"
+                      />
+                      <h2 className="text-sm">{user.name}</h2>
+                    </div>
+
+                    {/* Show host badge if exists */}
+                    {user.host && (
+                      <span className="text-xs px-3 py-0.5 rounded-full text-gray-base font-medium shadow-sm  border">
+                        Host
+                      </span>
+                    )}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-        </GlobalModal>
-      </div>
+        </div>
+      </GlobalModal>
     </div>
   );
 }
