14877cf8b472be0b2195cb085df27a388ac8a330
sallah10 <muhammedbello030@gmail.com>
2025-09-05 15:19:10 +0100

    All pages setup done

---
+++ apps/web/components/bookings/Booking.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import Image from "next/image";
+
+const Booking = () => {
+  return (
+    <>
+      <div>
+        <p className="text-[#98A2B3] text-md">20 minutes ago</p>
+        <h1 className="text-[#2A1D52] text-xl font-semibold">
+          Racoon Musicals {">"}
+        </h1>
+        <h2 className="text-[#C96FFF] text-xl">$1,500 per day</h2>
+        <p>
+          We’re hosting a corporate event and would like you to consider our
+          offer to rent your hall for 2 days at{" "}
+          <span className="text-[#C96FFF]">... more</span>
+        </p>
+        <div>{/* <small><span><Image/></span>May 1-2</small> */}</div>
+      </div>
+    </>
+  );
+};
+
+export default Booking;
